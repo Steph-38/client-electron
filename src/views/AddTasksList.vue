a79aa9bed562bcc2bc7cb407b9271fd8f22b0cfe
Steph-38 <stephchevalier38@gmail.com>
2020-09-25 15:05:50 +0200

    second commit

--- src/views/AddTasksList.vue
+++ src/views/AddTasksList.vue
@@ -1,13 +1,32 @@
 <template>
   <div class="about">
     <div class="container">
+      <!-- Affichage de l'erreur si champ vide -->
+      <div v-if="errorList != ''" class="alert alert-dismissible alert-danger">
+        <button type="button" class="close" data-dismiss="alert">&times;</button>
+        <strong>{{ errorList }}</strong>
+      </div>
+      <!-- Formulaire de création d'une liste -->
       <form action="" method="post">
         <div class="form-group">
           <label for="title">Titre de la liste</label>
           <input type="text" class="form-control" id="title" v-model="title">
         </div>
-        <button type="button" class="btn btn-outline-info" @click="addTaskList">Ajouter la liste</button>
+        <button type="submit" class="btn btn-outline-info" @click="addTasksList">Ajouter la liste</button>
       </form>
+      <!-- Affichage de l'erreur si champ vide -->
+      <!-- <div v-if="errorTask != ''" class="alert alert-dismissible alert-danger">
+        <button type="button" class="close" data-dismiss="alert">&times;</button>
+        <strong>{{ errorTask }}</strong>
+      </div> -->
+      <!-- Formulaire d'ajout d'un item si liste existe -->
+      <!-- <form action="" method="post">
+        <div class="form-group">
+          <label for="title">Titre de la tache</label>
+          <input type="text" class="form-control" id="title" v-model="task">
+        </div>
+        <button type="submit" class="btn btn-outline-info" @click="addTask(listTasks.id)">Ajouter une tache</button>
+      </form> -->
     </div>
   </div>
 </template>
@@ -16,22 +35,49 @@
 import axios from 'axios'
 
 export default {
-  data() {
+  data () {
     return {
       title: '',
-      
+      task: '',
+      listTasks: {},
+      errorList: '',
+      errorTask: []
     }
   },
 
   methods: {
-    addTaskList: async function() {
-		  await axios.post('http://localhost:3000/api/v1/list', {
-        title: this.title
-      })
-		  .then(response => console.log(response))
-			.catch(error => console.log(error));
+    addTasksList: async function () {
+      if (!this.title) {
+        this.errorList = 'Le champ titre ne doit pas être vide'
+      } else {
+        await axios.post('http://localhost:3000/api/v1/list', {
+          title: this.title
+        })
+          .then(response => console.log(response),
+            // response => this.listTasks,
+            this.errorList = '')
+          .catch(error => console.log(error))
+      }
     }
+
+    // addTask: async function (id) {
+    //   if (!this.task) {
+    //     this.errorTask.push({ msg: 'Le champ titre ne doit pas être vide' })
+    //   }
+    //   if (!this.listTasks) {
+    //     this.errorTask.push({ msg: 'Il n\'y a pas de liste créée' })
+    //   }
+
+    //   if (!this.errorTask) {
+    //     await axios.post(`http://localhost:3000/api/v1/list/${id}/item`, {
+    //       task: this.task
+    //     })
+    //       .then(response => console.log(response),
+    //         this.errorTask = '')
+    //       .catch(error => console.log(error))
+    //   }
+    // }
+
   }
 }
-
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <div class="container" v-for="listTask in listTasks" :key="listTask.id">
       <div class="card text-white bg-dark mb-3">
-        <div class="card-header">{{ listTask.title }} créée le {{ listTask.created_at }}</div>
+        <div class="card-header">{{ listTask.title }} créée le {{ listTask.created_at }} <button type="button" class="btn btn-danger" @click="deleteList(listTask.id)" @task-remove="refresh">Supprimer la liste</button></div>
         <div class="card-body">
           <div v-if="listTask.nb_item > 0">
             <a class="card-text" v-on:click="displayItems(listTask.id)" >{{ listTask.nb_item }} taches en cours</a>
@@ -44,10 +44,20 @@ export default {
   },
 
   methods: {
+
     displayItems: async function (i) {
       const response = await axios.get(`http://localhost:3000/api/v1/list/${i}/item`)
       this.listItems = response.data.map(listItem => ({ ...listItem, created_at: moment(listItem.created_at).format('DD/MM/YYYY') }))
-    } 
+    },
+    deleteList: async function (i) {
+      await axios.delete(`http://localhost:3000/api/v1/list/${i}`)
+        .then(response => this.refresh())
+        .catch(error => console.log(error))
+    },
+    refresh: async function () {
+      const response = await axios.get('http://localhost:3000/api/v1/list')
+      this.listTasks = response.data.map(listTask => ({ ...listTask, created_at: moment(listTask.created_at).format('DD/MM/YYYY') }))
+    }
   }
 }
 </script>
